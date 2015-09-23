b0a62e2c7e279ac9a4d953a08aac95f7ef247c60
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2015-09-23 07:25:42 +0530

    Initial commit

---
+++ server/src/main/java/com/project/App.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
